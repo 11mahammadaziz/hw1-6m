d79ea851b9e729ce2e033c82bef618a849d42556
11mahammadaziz <rustamjanov001@gmail.com>
2025-07-15 17:28:55 +0600

    //

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/home/Home';
+import { Posts } from './components/posts/Posts';
+import { Users } from './components/users/Users';
+import { Header } from './components/header/Header';
+import './styles/index.css';
+
+const queryClient = new QueryClient();
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<Posts />} />
+            <Route path='/users' element={<Users />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
